9da663ec9a4ac3bbbfa3246a11fdffee18eb4f14
ofgonen_office <ofgonen@gmail.com>
2023-03-24 17:42:16 +0300

    Implement Vector2GameEventResponse

--- Assets/Script/FFStudio/Event/EventListenerGenericUnityEventResponse.cs
+++ Assets/Script/FFStudio/Event/EventListenerGenericUnityEventResponse.cs
@@ -91,6 +91,14 @@ namespace FFStudio
 		}
 	}
 
+	public class Vector2GameEventResponse : GenericEventListenerGenericUnityEventResponse< Vector2GameEvent, Vector2 >
+	{
+		protected override void OnEventResponse()
+		{
+			unityEvent.Invoke( gameEvent.eventValue );
+		}
+	}
+
 	public class ReferenceGameEventResponse : GenericEventListenerGenericUnityEventResponse< ReferenceGameEvent, object >
 	{
 		protected override void OnEventResponse()

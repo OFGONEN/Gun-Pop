28714ebed3f637db1254015e9ae344a3bb56eb7b
ofgonen_office <ofgonen@gmail.com>
2023-03-24 11:29:45 +0300

    LevelData: Data percentage of gun spawn data

--- Assets/Script/FFStudio/LevelData.cs
+++ Assets/Script/FFStudio/LevelData.cs
@@ -15,6 +15,10 @@ namespace FFStudio
 		[ ValueDropdown( "SceneList" ), LabelText( "Scene Index" ) ] public int scene_index;
         [ LabelText( "Override As Active Scene" ) ] public bool scene_overrideAsActiveScene;
 
+	[ Title( "Level" ) ]
+		[ LabelText( "Gun Color Data Percentage " ) ] public DataPercentageGunColorData[] data_gunColor_percentage_array;
+		[ LabelText( "Gun Data Percentage " ) ] public DataPercentageGunData[] data_gun_percentage_array;
+
 #if UNITY_EDITOR
 		static IEnumerable SceneList()
         {
@@ -29,4 +33,18 @@ namespace FFStudio
 		}
 #endif
     }
+}
+
+[ System.Serializable ]
+public struct DataPercentageGunColorData
+{
+	public int data_percentage;
+	public GunColorData data;
+}
+
+[ System.Serializable ]
+public struct DataPercentageGunData
+{
+	public int data_percentage;
+	public GunData data;
 }

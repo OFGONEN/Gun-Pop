01b5cc1357d9eca4a8cfa4d5a8b0e0f82fa00c6d
ofgonen_office <ofgonen@gmail.com>
2023-03-24 16:49:37 +0300

    Implement SelectionLine

---
+++ Assets/Script/SelectionLine.cs
@@ -0,0 +1,53 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Shapes;
+using Sirenix.OdinInspector;
+
+public class SelectionLine : MonoBehaviour
+{
+#region Fields
+  [ Title( "Shared" ) ]
+    [ SerializeField ] PoolSelectionLine pool_selection_line;
+
+  [ Title( "Components" ) ]
+    [ SerializeField ] Line _lineRenderer;
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+    public void Spawn( Vector3 position, Color color )
+    {
+		gameObject.SetActive( true );
+
+		_lineRenderer.Start = position;
+		_lineRenderer.End   = position;
+		_lineRenderer.Color = color;
+	}
+
+    public void UpdateLine( Vector3 position )
+    {
+		_lineRenderer.End = position;
+	}
+
+    public void OnDeSpawnLines()
+    {
+		pool_selection_line.ReturnEntity( this );
+	}
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}

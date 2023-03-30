c3d95279e8714f5e8158f90706782a987f06329d
ofgonen_office <ofgonen@gmail.com>
2023-03-30 16:45:27 +0300

    SystemMerge: Initial merge sequence

--- Assets/Script/SystemMerge.cs
+++ Assets/Script/SystemMerge.cs
@@ -13,7 +13,9 @@ public class SystemMerge : ScriptableObject
   [ Title( "Shared" ) ]
     [ SerializeField ] RunTimeSetGun set_gun;
     [ SerializeField ] GameEvent event_selection_enable;
-      
+
+    int merge_index;
+    Gun merge_gun;
 #endregion
 
 #region Properties
@@ -26,17 +28,44 @@ public class SystemMerge : ScriptableObject
     public void OnSelectionComplete()
     {
         if( set_gun.itemList.Count >= GameSettings.Instance.merge_count )
-			DoMerge();
+			StartMerge();
         else
 			event_selection_enable.Raise();
 	}
 #endregion
 
 #region Implementation
-    void DoMerge()
+    void StartMerge()
     {
+		merge_index = 1;
+		merge_gun   = set_gun.itemList[ 0 ];
 
-    }
+		merge_gun.DoMerge( set_gun.itemList[ merge_index ], OnMergeDone );
+	}
+
+    void ContiuneMerge()
+    {
+		merge_index++;
+		merge_gun.DoMerge( set_gun.itemList[ merge_index ], OnMergeDone );
+	}
+
+    void OnMergeDone()
+    {
+		set_gun.itemList[ merge_index ].OnMerged();
+		merge_gun.DoUpgrade();
+        // Remove selection line
+
+		if( merge_index == set_gun.itemList.Count - 1 )
+			OnMergeSequenceComplete();
+        else
+			ContiuneMerge();
+	}
+
+    void OnMergeSequenceComplete()
+    {
+		// lift the gun up and fire
+		merge_gun.DoFire();
+	}
 #endregion
 
 #region Editor Only

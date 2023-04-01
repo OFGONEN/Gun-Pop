42455d927102a21bfe641d037defdf694e32058f
ofgonen_office <ofgonen@gmail.com>
2023-04-01 17:15:16 +0300

    Enemy: Gun fire start and damage sequences

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FFStudio;
+using DG.Tweening;
 using Sirenix.OdinInspector;
 
 public class Enemy : MonoBehaviour
@@ -21,10 +22,14 @@ public class Enemy : MonoBehaviour
     [ SerializeField ] Material enemy_material_dead;
 
   [ Title( "Components" ) ]
+	[ SerializeField ] Transform enemy_gfx_transform;
 	[ SerializeField ] MeshRenderer enemy_body_renderer;
 	[ SerializeField ] MeshRenderer enemy_crown_renderer;
 
     int health_current;
+	Vector3 enemy_position_start;
+
+	RecycledTween recycledTween = new RecycledTween();
 #endregion
 
 #region Properties
@@ -34,26 +39,62 @@ public class Enemy : MonoBehaviour
     void Start()
     {
 		notif_level_progress.SharedValue = 1;
-		health_current = CurrentLevelData.Instance.levelData.enemy_health;
+		health_current                   = CurrentLevelData.Instance.levelData.enemy_health;
+
+		enemy_position_start = transform.position;
 	}
 
 
 #endregion
 
 #region API
-    public void OnGunFire( int damage )
+	public void OnGunFireStart()
+	{
+		recycledTween.Recycle( transform.DOMove(
+			notif_enemy_fire_position.sharedValue,
+			GameSettings.Instance.enemy_move_duration )
+			.SetEase( GameSettings.Instance.enemy_move_ease )
+		);
+	}
+
+    public void OnGunFired( int damage )
     {
 		health_current                   = Mathf.Max( health_current - damage,  0 );
 		notif_level_progress.SharedValue = Mathf.InverseLerp( 0, CurrentLevelData.Instance.levelData.enemy_health, health_current );
 
         if( health_current > 0 )
-			event_enemy_damaged.Raise();
-        else
-			event_enemy_died.Raise();
+			EnemyDamagedSequence();
+		else
+			EnemyDiedSequence();
 	}
 #endregion
 
 #region Implementation
+	void EnemyDamagedSequence()
+	{
+		enemy_body_renderer.sharedMaterial = enemy_material_damaged;
+		recycledTween.Recycle( GameSettings.Instance.enemy_damage_punchScale.CreateTween( enemy_gfx_transform ), OnEnemyDamagedSequenceComplete );
+	}
+
+	void EnemyDiedSequence()
+	{
+		enemy_body_renderer.sharedMaterial  = enemy_material_dead;
+		enemy_crown_renderer.sharedMaterial = enemy_material_dead;
+
+		event_enemy_died.Raise();
+	}
+
+	void OnEnemyDamagedSequenceComplete()
+	{
+		enemy_body_renderer.sharedMaterial = enemy_material_idle;
+		event_enemy_damaged.Raise();
+
+		recycledTween.Recycle( transform.DOMove(
+			enemy_position_start,
+			GameSettings.Instance.enemy_move_duration )
+			.SetEase( GameSettings.Instance.enemy_move_ease )
+		);
+	}
 #endregion
 
 #region Editor Only
